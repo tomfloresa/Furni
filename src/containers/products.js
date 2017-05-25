1925c4beec4f33d011082bbe142a8f20f86970ed
Tomás Andrés Flores Alvear <tomas.flores.a@gmail.com>
2017-05-25 18:04:40 -0400

    woring in product

--- src/containers/products.js
+++ src/containers/products.js
@@ -11,12 +11,32 @@ class Products extends Component {
     this.props.fetchProducts(id);
   }
 
+  getHoverImages(product) {
+    return _.map(product.product_images, image => {
+      console.log(image.index);
+      return (
+        <img src={image.image_url} alt="" key={image.image_url}/>
+      );
+    });
+  }
+
+  playImages() {
+    console.log("TEst");
+  }
+
+  stopAndBackToBegin() {
+    console.log("TEst");
+  }
+
   showProducts() {
     return _.map(this.props.products, product => {
       return (
         <div key={product.id} className="col-lg-4 col-md-4 product-showcase">
           <Link to={`productos/${product.id}`}>
             <div className="product-showcase-inner">
+              <div className="images-product-hover" onMouseEnter={this.playImages} onMouseLeave={this.stopAndBackToBegin}>
+                {this.getHoverImages(product)}
+              </div>
               <h3 className="text-center">{product.name}</h3>
               <p className="product-description">{product.description}</p>
               <h5 className="product-availability">{product.available ? 'Disponible' : 'No Disponible'}</h5>
